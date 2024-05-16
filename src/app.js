41cd5989ff63dc73d56601d6fe3ee527b9aa0f90
Rodolfo Escaleira <74121364+eXclus1on@users.noreply.github.com>
2024-05-16 21:24:15 +0100

    "Updated port assignment in app.js to use 3000 as default value"

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const movieRouter = require("./routes/movieRouter");
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
